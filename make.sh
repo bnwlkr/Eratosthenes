c12febc9104bd655a5789bb96684c941ecff96a7
Ben Walker <bnwlkr@Bens-MacBook-Pro.local>
2019-05-14 00:25:07 -0700

    turns out that the sieve had already been implemented, so instead I am going to implement a simple fibonacci sequence generator

---
+++ make.sh
@@ -0,0 +1,2 @@
+gcc -c -Ilibtask/ sieve_co.c
+gcc -o sieve_co sieve_co.o libtask/libtask.a
--- sieve_co.c
+++ sieve_co.c
@@ -1,14 +1,20 @@
-#include <stdlib.h>
+/* Copyright (c) 2005 Russ Cox, MIT; see COPYRIGHT */
+
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <task.h>
 
 
 
 
-int main () {
+void 
 
-  
-  
-  return 0;
+
+
+void taskmain () {
+
+
+
+
 }
